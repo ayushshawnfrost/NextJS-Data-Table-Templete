17b10ba82ad89885495183bf69f39e045b8f9e30
ayushshawnfrost <ayushmarkevens@gmail.com>
2024-06-13 19:59:43 -0700

    FEAT: added new rout /chat to the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -23,15 +23,25 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className="mb-3 text-2xl font-semibold"> Available routs:{" "}</h2>
-        </a><Link
+          <h2 className="mb-3 text-2xl font-semibold"> Available routs: </h2>
+        </a>
+        <Link
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           href="/users"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-           
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;/users
+            </span>
+          </h2>
+        </Link>
+        <Link
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          href="/chat"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;/chat
             </span>
           </h2>
         </Link>
